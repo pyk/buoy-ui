bdf30642237aa05e939612408b82b46d27f0f3ea
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-02-07 11:14:54 +0700

    initalize landing pages

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("span.header-content-txt-typed").typed({
-        strings: ["Knowledge base^1000", "FAQ pages^1000", "Get started pages^1000", "Help pages^1000", "Documentations^1000"],
-        typeSpeed: 50,
-        backSpeed: 20,
-        loop: true
-    });
-});
